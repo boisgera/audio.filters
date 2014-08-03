569d2d3356810cc932cf78fbad56a927ea793f2a
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2014-08-03 16:24:49 +0200

    bench for 1sec of single audio CD channel

--- bench.py
+++ bench.py
@@ -6,10 +6,13 @@ import docbench
 def bench():
     """
     >>> import numpy as np
-    >>> fir = audio.filters.FIR(0.1 * np.ones(100))
-    >>> input = np.ones(100000)
+    >>> fir = audio.filters.FIR(a=np.ones(512))
+    >>> input = np.ones(44100)
     >>> output = fir(input)
     """
 
 if __name__ == "__main__":
+    # Warning: the gc disabling can generate memory errors when a lot
+    # of data is moved around. The gc should probably be not be disabled
+    # by default.
     docbench.benchmod()
